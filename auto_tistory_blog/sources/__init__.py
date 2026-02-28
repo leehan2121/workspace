8f693f510ed3c1a99ae41edc26f0f67e958b0b91
leehan2121 <leehan2121@gmail.com>
2026-02-28 18:28:01 +0900

    #aaa

--- auto_tistory_blog/sources/__init__.py
+++
@@ -1,1 +0,0 @@
-# sources/__init__.py
--- baccarat/main.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-실제 GUI 실행용 main.py
-"""
-from gui.app import BaccaratApp
-
-if __name__ == "__main__":
-    app = BaccaratApp()
-    app.run()
